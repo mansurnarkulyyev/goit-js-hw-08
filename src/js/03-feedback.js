81e13dab4513764dd309f9d931d06426422e9661
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-12-20 17:25:55 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,14 @@
 // Описан в документации
 import throttle from "lodash.throttle";
 
-
 const CONTACT_FORM_LOCAL_STORAGE_KEY = 'feedback-form-state';
 const contactFormEl = document.querySelector('.feedback-form');
-// const contactFormInput = document.querySelector('.feedback-form input');
 const contactFormTextarea = document.querySelector('.feedback-form textarea');
 const formData = {};
-
 
 const fillContactFormElements = form => {
     const formDataFromLocalStorage = JSON.parse(localStorage.getItem(CONTACT_FORM_LOCAL_STORAGE_KEY));
     const formElements = form.elements;
-    // console.log(formElements);
-    // console.log(formElements['email'].value);
     for (const key in formDataFromLocalStorage) {
         if (formDataFromLocalStorage.hasOwnProperty(key)) {
             formElements[key].value = formDataFromLocalStorage[key];
@@ -21,7 +16,6 @@ const fillContactFormElements = form => {
     }
 
 };
-
 
 fillContactFormElements(contactFormEl);
 
@@ -32,31 +26,17 @@ const onContactFormElChange = event => {
     const contactFormElName = target.name;
 
     formData[contactFormElName] = contactFormElValue;
-
-    // console.log(JSON.stringify(formData));
-
     localStorage.setItem(CONTACT_FORM_LOCAL_STORAGE_KEY, JSON.stringify(formData));
-
 };
 
 const onContactFormSubmit = event => {
     event.preventDefault();
-
-    // const contactFormElValue = target.value;
-    // const contactFormElName = target.name;
-    // const contactFormElValue = currentTarget.value;
-    // const contactFormElName = currentTarget.name;
-
-    // formData[contactFormElName] = contactFormElValue;
-    // console.log(formData);
 
     const formData = { email: contactFormEl.email.value, message: contactFormEl.message.value }
     console.log(formData)
 
     localStorage.removeItem(CONTACT_FORM_LOCAL_STORAGE_KEY);
     event.currentTarget.reset();
-    // console.log(JSON.stringify(formData));
-
 };
 
 contactFormTextarea.addEventListener('change', throttle(onContactFormElChange, 500));
